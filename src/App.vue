2d05ee13820b47349ae21fd330a3bcfce464479a
joaofelipe <joaofelipe1294@gmail.com>
2022-01-14 19:24:16 -0300

    feat: add products load.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div>
+      <h1>Bem vindo</h1>
+    </div>
   </div>
 </template>
 
@@ -24,8 +11,15 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      products: []
     }
+  },
+
+  created() {
+    let promise = this.$http.get('http://localhost:3000/api/products')
+    promise.then(response => response.json())
+            .then(products => this.products = products)
+            .catch(err => console.log(err))
   }
 }
 </script>
